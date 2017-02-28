2ff37c85042bae1204fd310860be079263d9ca84
gauravjeet <gauravjeet007@gmail.com>
2017-02-28 19:35:32 +0530

    fixed plugin support for oc-contrib-plugin

--- src/Composer/OcContrib/ContribInstaller.php
+++ src/Composer/OcContrib/ContribInstaller.php
@@ -12,21 +12,12 @@ class ContribInstaller extends LibraryInstaller {
    */
   public function getInstallPath(PackageInterface $package) {
     return 'gjsingh/';
-//    $prefix = substr($package->getPrettyName(), 0, 23);
-//    if ('/template-' !== $prefix) {
-//      throw new \InvalidArgumentException(
-//        'Unable to install template, phpdocumentor templates '
-//        .'should always start their package name with '
-//        .'"phpdocumentor/template-"'
-//      );
-//    }
-//    return 'gjsingh/' . substr($package->getPrettyName(), 23);
   }
 
   /**
    * {@inheritDoc}
    */
   public function supports($packageType) {
-    return 'thissssss';//'oc-contrib-plugin' === $packageType;
+    return 'oc-contrib-plugin' === $packageType;
   }
 }
